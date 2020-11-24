c29d41c4601b99796439b611fb32aa563dbaeeae
Crowdbotics <team@crowdbotics.com>
2020-11-24 15:28:20 +0000

    Update admin title

--- onlineheakthcare_22951/urls.py
+++ onlineheakthcare_22951/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "onlineheakthcare"
-admin.site.site_title = "onlineheakthcare Admin Portal"
-admin.site.index_title = "onlineheakthcare Admin"
+admin.site.site_header = "onlinehealthcare"
+admin.site.site_title = "onlinehealthcare Admin Portal"
+admin.site.index_title = "onlinehealthcare Admin"
 
 # swagger
 api_info = openapi.Info(
